ae670970f5b78dccb75d999651fd833e2eb3aba8
plgro2 <pierre-luc.grondin.1@ulaval.ca>
2017-11-06 10:47:41 -0500

    Add title to temperature graph

--- R/plot_temperature.R
+++ R/plot_temperature.R
@@ -3,6 +3,7 @@ df = read_csv("data/clean/T_Antalya_EM.csv")
 
 df%>%
   ggplot(aes(x = month, y = temperature))+
-  geom_boxplot()
+  geom_boxplot()+
+  ggtitle("This is a new title")
 
 ggsave("graphs/boxplot_temperature.pdf")
