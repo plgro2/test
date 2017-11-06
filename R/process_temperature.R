0222b160dced900366b7cbb1fa8f351b64efebd8
plgro2 <pierre-luc.grondin.1@ulaval.ca>
2017-11-06 11:46:01 -0500

    changed titles in xlsx

--- R/process_temperature.R
+++ R/process_temperature.R
@@ -1,7 +1,7 @@
 
 df = read_csv(file = "data/raw/T_Antalya_EM.csv")
 
-df = gather(df, month, temperature,-tplace, -year)
+df = gather(df, mois, temperature,-tplace, -year)
 
 write_csv(df, "data/clean/T_Antalya_EM.csv")
 write_excel_csv(df, "data/clean/T_Antalya_EM.xlsx")
