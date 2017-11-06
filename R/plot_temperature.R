e63bfd64f353b5750e4461b1c53f1f4f4c41bdea
plgro2 <pierre-luc.grondin.1@ulaval.ca>
2017-11-06 11:26:18 -0500

    Updated version

--- R/plot_temperature.R
+++ R/plot_temperature.R
@@ -4,6 +4,6 @@ df = read_csv("data/clean/T_Antalya_EM.csv")
 df%>%
   ggplot(aes(x = month, y = temperature))+
   geom_boxplot()+
-  ggtitle("My title is much better!")
+  ggtitle("My title is much better! - Are you sure?")
 
 ggsave("graphs/boxplot_temperature.pdf")
