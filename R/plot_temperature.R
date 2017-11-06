a60288ab300a25590b458b9854a5d2d6da55fbb1
Philippe Massicotte <philippe.massicotte@takuvik.ulaval.ca>
2017-11-06 11:29:54 -0500

    New title

--- R/plot_temperature.R
+++ R/plot_temperature.R
@@ -4,6 +4,6 @@ df = read_csv("data/clean/T_Antalya_EM.csv")
 df%>%
   ggplot(aes(x = month, y = temperature))+
   geom_boxplot()+
-  ggtitle("My title is much better! - Are you sure?")
+  ggtitle("My title is much better! - Are you sure?, yes I am")
 
 ggsave("graphs/boxplot_temperature.pdf")
